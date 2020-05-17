521b9b6a4dc42c3408da8aaa0f9ba741b6f6cbca
michmitch <62762271+michmitch@users.noreply.github.com>
2020-05-18 01:44:56 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,7 @@
-var CACHE_STATIC_NAME = 'static-v13';
-var CACHE_DYNAMIC_NAME = 'dynamic-v2';
-
-self.addEventListener('install', function (event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_STATIC_NAME)
-      .then(function (cache) {
-        console.log('[Service Worker] Precaching App Shell');
+    caches.open('first-app')
+      .then(function(cache) {
         cache.addAll([
           '/',
           '/index.html',
@@ -34,132 +29,17 @@ self.addEventListener('install', function (event) {
           '/img/icons/icon-192x192.png',
           '/img/icons/icon-384x384.png',
           '/img/icons/icon-512x512.png'
-
-          // '/src/js/app.js',
-          // '/src/js/feed.js',
-          // '/src/js/promise.js',
-          // '/src/js/fetch.js',
-          // '/src/js/material.min.js',
-          // '/src/css/app.css',
-          // '/src/css/feed.css',
-          // '/src/images/main-image.jpg',
-          // 'https://fonts.googleapis.com/css?family=Roboto:400,700',
-          // 'https://fonts.googleapis.com/icon?family=Material+Icons',
-          // 'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-        ]);
-      })
-  )
-});
-
-self.addEventListener('activate', function (event) {
-  console.log('[Service Worker] Activating Service Worker ....', event);
-  event.waitUntil(
-    caches.keys()
-      .then(function (keyList) {
-        return Promise.all(keyList.map(function (key) {
-          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-            console.log('[Service Worker] Removing old cache.', key);
-            return caches.delete(key);
-          }
-        }));
+        ])
       })
   );
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function (event) {
-  var url = 'https://httpbin.org/get';
-
-  if (event.request.url.indexOf(url) > -1) {
-    event.respondWith(
-      caches.open(CACHE_DYNAMIC_NAME)
-        .then(function (cache) {
-          return fetch(event.request)
-            .then(function (res) {
-              cache.put(event.request, res.clone());
-              return res;
-            });
-        })
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function (response) {
-          if (response) {
-            return response;
-          } else {
-            return fetch(event.request)
-              .then(function (res) {
-                return caches.open(CACHE_DYNAMIC_NAME)
-                  .then(function (cache) {
-                    cache.put(event.request.url, res.clone());
-                    return res;
-                  })
-              })
-              .catch(function (err) {
-                return caches.open(CACHE_STATIC_NAME)
-                  .then(function (cache) {
-                    return cache.match('/offline.html');
-                  });
-              });
-          }
-        })
-    );
-  }
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(res) {
+        return res;
+      })
+  );
 });
-
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request)
-//       .then(function(response) {
-//         if (response) {
-//           return response;
-//         } else {
-//           return fetch(event.request)
-//             .then(function(res) {
-//               return caches.open(CACHE_DYNAMIC_NAME)
-//                 .then(function(cache) {
-//                   cache.put(event.request.url, res.clone());
-//                   return res;
-//                 })
-//             })
-//             .catch(function(err) {
-//               return caches.open(CACHE_STATIC_NAME)
-//                 .then(function(cache) {
-//                   return cache.match('/offline.html');
-//                 });
-//             });
-//         }
-//       })
-//   );
-// });
-
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     fetch(event.request)
-//       .then(function(res) {
-//         return caches.open(CACHE_DYNAMIC_NAME)
-//                 .then(function(cache) {
-//                   cache.put(event.request.url, res.clone());
-//                   return res;
-//                 })
-//       })
-//       .catch(function(err) {
-//         return caches.match(event.request);
-//       })
-//   );
-// });
-
-// Cache-only
-// self.addEventListener('fetch', function (event) {
-//   event.respondWith(
-//     caches.match(event.request)
-//   );
-// });
-
-// Network-only
-// self.addEventListener('fetch', function (event) {
-//   event.respondWith(
-//     fetch(event.request)
-//   );
-// });
